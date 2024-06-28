6eacc6cc57142a9bfe4283dc19f7c6c909d1409d
ymukhame <qmerty1235@gmail.com>
2024-06-28 17:14:24 +0500

    get state instance

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"text/template"
 )
+
+type TemplateData struct {
+	Resp string
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "template.html")
+		resp := r.URL.Query().Get("resp")
+		data := &TemplateData{Resp: resp}
+		tmpl, _ := template.ParseFiles("template.html")
+		tmpl.Execute(w, data)
 	})
 	http.HandleFunc("/settings", GetSettings)
 	http.HandleFunc("/state_instance", GetStateInstance)
@@ -18,7 +27,6 @@ func main() {
 }
 
 func GetSettings(w http.ResponseWriter, r *http.Request) {
-
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Failed to parse form", http.StatusBadRequest)
@@ -26,13 +34,13 @@ func GetSettings(w http.ResponseWriter, r *http.Request) {
 	}
 	idInstance := r.FormValue("idInstance")
 	token := r.FormValue("apiToken")
-	fmt.Println("DATA", idInstance, token)
-	url := fmt.Sprintf("https://7103.api.greenapi.com/waInstance%s/getSettings/%s", idInstance, token)
-	body, err := http.Post(url, "application/json", nil)
+	link := fmt.Sprintf("https://7103.api.greenapi.com/waInstance%s/getSettings/%s", idInstance, token)
+	body, err := http.Get(link)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer body.Body.Close()
 	resp, err := io.ReadAll(body.Body)
 	if err != nil {
 		fmt.Println(err)
@@ -40,9 +48,34 @@ func GetSettings(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println(string(resp))
 
+	encodedResp := url.QueryEscape(string(resp))
+	http.Redirect(w, r, "/?resp="+encodedResp, http.StatusSeeOther)
 }
-func GetStateInstance(w http.ResponseWriter, r *http.Request) {
 
+func GetStateInstance(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
+	idInstance := r.FormValue("idInstance")
+	token := r.FormValue("apiToken")
+	link := fmt.Sprintf("https://7103.api.greenapi.com/waInstance%s/getStateInstance/%s", idInstance, token)
+	body, err := http.Get(link)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer body.Body.Close()
+	resp, err := io.ReadAll(body.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(resp))
+
+	encodedResp := url.QueryEscape(string(resp))
+	http.Redirect(w, r, "/?resp="+encodedResp, http.StatusSeeOther)
 }
 
 func SendMessage(w http.ResponseWriter, r *http.Request) {
@@ -50,5 +83,5 @@ func SendMessage(w http.ResponseWriter, r *http.Request) {
 }
 
 func SendFile(w http.ResponseWriter, r *http.Request) {
-
+	// Ваша логика для SendFile
 }
